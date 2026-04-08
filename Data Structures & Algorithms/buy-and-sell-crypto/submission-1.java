3158550a87660f9af05a3a98ddbefda893e56765
Ramcharan2012 <153090162+Ramcharan2012@users.noreply.github.com>
2026-04-08 22:08:09 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int p=0;
+        int min=prices[0];
+        for(int i=1;i<prices.length;i++){
+            min=Math.min(prices[i],min);
+            p=Math.max(p,prices[i]-min);
+        }
+        return p;
+    }
+}
