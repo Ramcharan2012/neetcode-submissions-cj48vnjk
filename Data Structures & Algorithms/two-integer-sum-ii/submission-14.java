15eeaf5730d239d21a66adf1f63ff82cc66a723a
Ramcharan2012 <153090162+Ramcharan2012@users.noreply.github.com>
2026-04-19 23:35:56 +0530

    Update: two-integer-sum-ii - submission-14

--- Data Structures & Algorithms/two-integer-sum-ii/submission-14.java
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-14.java
@@ -1,20 +1,18 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-       int left = 0;
-int right = numbers.length - 1;
+        int i = 0, j = numbers.length - 1, sum = 0;
+        while (i < j) {
+            sum = numbers[i] + numbers[j];
+            if (sum == target) {
+                return new int[] {i+1,j+1};
+            }
+            if (sum > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return new int []{};
+    }
 
-while (left < right) {
-    int currentSum = numbers[left] + numbers[right];
-    if (currentSum == target) {
-        return new int[]{left+1,right+1};
-    } else if (currentSum < target) {
-        // Need a larger sum
-        left++;
-    } else {
-        // Need a smaller sum
-        right--;
-    }
-}
-        return new int[]{};
-    }
 }
